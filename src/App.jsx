5ed13a36bb372674a95b3472d080a13b798e1242
LittigDev <littig.develop@gmail.com>
2023-01-29 16:57:05 -0300

    FIR/01 Add header component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './global.css';
+import { Header } from './components/Header.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <h1>Ignite Feed Project</h1>
+    </div>
+  )
+}
+
+export default App
