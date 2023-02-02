5adafbf211dd468d301b131c40346ba6c5dd00c9
LittigDev <littig.develop@gmail.com>
2023-02-01 22:06:30 -0300

    FIR/07 add props in postcomponent and turning it into dynamic component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,50 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
 //TODO: Ajustar cor do valor escrito em textarea;
 
-export function Post(props) {
-    console.log('Props the post', props)
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    });
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    });
 
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={props.author.avatarUrl}/>
+                    <Avatar src={author.avatarUrl}/>
                     <div className={styles.authorInfo}>
-                        <strong>{props.author.name}</strong>
-                        <span>{props.author.role}</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="31 de Janeiro às 08:00h" dateTime="2023-01-31 08:00:00">Públicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    { publishedDateRelativeToNow }
+                </time>
             </header>
+
             <div className={styles.content}>
-                <p>Fala galeraa 👋🏼</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat</p>
-                <p>👉🏼 <a href="#">jane.desing/doctorcare</a></p>
-                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+                {
+                    content.map(item => {
+                        if (item.type === 'paragraph') {
+                            return <p>{ item.content }</p>
+                        } else if (item.type === 'link') {
+                            return <p><a href="#">{ item.content }</a></p>
+                        }
+
+                        return (
+                            <p>Post sem conteúdo</p>
+                        )
+                    })
+                }
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
