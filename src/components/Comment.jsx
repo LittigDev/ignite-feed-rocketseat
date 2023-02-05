b4bc1864ab64911604da0e0df5194d28ff5520e8
LittigDev <littig.develop@gmail.com>
2023-02-05 16:34:19 -0300

    FIR/12 Adding count likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         console.log('Remover comentário');
         onDeleteComment(content);
     };
+
+    function handleLikeComment () {
+        setLikeCount(likeCount + 1);
+    }
 
     return (
         <div className={styles.comment}>
@@ -27,12 +34,12 @@ export function Comment({ content, onDeleteComment }) {
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={() => setLikeCount(likeCount + 1)}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
-            </div>
+            </div> 
         </div>
     )
 }
